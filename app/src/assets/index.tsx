870eb77b0d7cfeb6cbcaa43730db50d6e573352a
Bobby Christopher <bobby@ixisdigital.com>
2021-07-27 19:58:17 -0400

    added the previews

--- app/src/assets/index.tsx
+++ app/src/assets/index.tsx
@@ -1,8 +1,6 @@
 import terminalButtons from './terminalButtons.svg';
 import justHacker  from './justHacker.svg'
-import profileSquare  from './profileSquare.jpeg'
-import messageFrameDemo  from './messageFrameDemo.gif'
 
 
 
-export { terminalButtons, justHacker, profileSquare, messageFrameDemo }
+export { terminalButtons, justHacker }
