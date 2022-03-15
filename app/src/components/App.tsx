0921a7296950908abcf9c2978ef708dd369383fa
Bobby Christopher <bobby@ixisdigital.com>
2022-03-15 18:28:05 -0400

    added resume button and ordered my projects

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -2,6 +2,8 @@ import {FC} from 'react';
 import styled from 'styled-components';
 import { Projects,Skills,Intro } from './Sections'
 import {justHackerCircle }  from '../assets';
+import RubberBand from 'react-reveal/RubberBand';
+
 
 
 const AppContainer = styled.div.attrs(() => ({
@@ -19,6 +21,15 @@ const Header = styled.div.attrs(() => ({
   className: `flex flex-row custom-purp items-center text-white justify-left w-full p-4 h-20 z-50`
   }))`
   `
+  const ResumeButton = styled.button.attrs(() => ({
+    className: `m-50 p-1 float-left shadow-lg rounded-sm	`
+    }))`
+      background-color: #8985a8;
+      &:hover {
+        background-color: #575374;
+      }
+
+    `
 
 
 export const App: FC = () => {
@@ -26,7 +37,18 @@ export const App: FC = () => {
     <>
       <AppContainer>
         <Header style={{position: 'fixed',top: 0, left: 0}}>
-          <img src={justHackerCircle} alt={'hackerProfile'} style={{height: '100%', margin: 10}}/> <p><b>  Bobby Christopher </b> - Full Stack Software Engineer </p>
+          <div style={{display: 'flex', flex: 1, textAlign: 'center',height: '100%', alignItems: 'center'}}>
+            <img src={justHackerCircle} alt={'hackerProfile'} style={{height: '100%', margin: 10}}/> 
+            <div style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent:'center', }} >
+              <b>  Bobby Christopher </b>  
+               <div className='invisible md:visible' > - Full Stack Software Engineer</div>
+            </div>
+          </div>
+          <div >
+            <RubberBand forever={true}>
+              <ResumeButton>Resumé</ResumeButton>
+            </RubberBand>
+          </div>
         </Header>
         <div style={{height: 80, width: '100%'}} />
 
