c9097dd3f137e1ce2ca30e7c47ce6a50f1241261
Bobby Christopher <bchristopher@shopmonkey.io>
2024-03-10 20:04:12 -0400

    remove api key

--- old/school/static/personal/css/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<h2>HEY!</h2>")
-
-def fuckDylan(request):
-    return HttpResponse("<h2>fuckDylan</h2>")
